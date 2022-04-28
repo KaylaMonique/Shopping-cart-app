50d1f54291ccef94ec3fe3be8b59923b3b69c055
KaylaMonique <102808151+KaylaMonique@users.noreply.github.com>
2022-04-28 11:18:26 +0200

    added classes and created name and price variables

---
+++ ShoppingCartAppSln/ShoppingLibrary/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingLibrary
+{
+    public class ShoppingCart
+    {
+        List<ShoppingCartItem> _ShoppingCartItem = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+
+        }
+    }
+}
---
+++ ShoppingCartAppSln/ShoppingLibrary/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingLibrary
+{
+    public class ShoppingCartItem
+    {
+        public string itemName;
+        public string itemPrice;
+        
+
+    }
+}
